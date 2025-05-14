dd3fd488b9532dc77db6ac29494344b884c34f44
Mik Stavast <mik.stavast@student.graafschapcollege.nl>
2025-05-14 08:54:55 +0200

    initial

---
+++ CovaDISAuto.Infrastructure/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CovaDISAuto.Infrastructure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
