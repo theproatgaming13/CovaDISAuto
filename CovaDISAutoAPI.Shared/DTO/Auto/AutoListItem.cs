127209710c703d889ef6a2d36affe18a23a8ed09
Mik Stavast <mik.stavast@student.graafschapcollege.nl>
2025-05-15 13:45:28 +0200

    dto map aan gemaakt

---
+++ CovaDISAutoAPI.Shared/DTO/Auto/AutoListItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CovaDISAutoAPI.Shared.DTO.Auto
+{
+    public class AutoListItem
+    {
+        public int id { get; set; }
+        public string? merk { get; set; }
+        public string? type { get; set; }
+        public string? kleur { get; set; }
+    }
+}
