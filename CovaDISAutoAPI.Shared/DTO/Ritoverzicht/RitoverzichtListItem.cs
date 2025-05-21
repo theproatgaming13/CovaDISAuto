337f132eeb352940189587d6a6e4f5c9253bb64b
frikandelcat <131359082+frikandelcat@users.noreply.github.com>
2025-05-21 09:39:09 +0200

    Repository begin (#13)

---
+++ CovaDISAutoAPI.Shared/DTO/Ritoverzicht/RitoverzichtListItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CovaDISAutoAPI.Shared.DTO.Ritoverzicht
+{
+    public class RitoverzichtListItem
+    {
+        public int ID { get; set; }
+        public int Kilometerstand_voor { get; set; }
+
+        // Later verwijderen
+        public int Chauffeur_id { get; set; }
+        public int Route_id { get; set; }
+    }
+}
