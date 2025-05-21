1bc2150c5771db6e85ef3029c1183687c2ebcb6e
Justin <justin.lepper@student.graafschapcollege.nl>
2025-05-21 09:38:07 +0200

    Repository begin

---
+++ CovaDISAutoAPI.API/Controllers/RitoverzichtController.cs
@@ -0,0 +1,23 @@
+﻿using CovaDISAutoAPI.Application.Repositories;
+using CovaDISAutoAPI.Shared.DTO.Ritoverzicht;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CovaDISAutoAPI.API.Controllers
+{
+    public class RitoverzichtController
+    {
+        private readonly RitoverzichtRepository ritoverzichtRepository;
+
+        public RitoverzichtController(RitoverzichtRepository ritoverzichtRepository)
+        {
+            this.ritoverzichtRepository = ritoverzichtRepository;
+        }
+
+        [HttpGet]
+        [Route("geefRitoverzichten")]
+        public ActionResult<IEnumerable<RitoverzichtListItem>> GeefAlleBoeken()
+        {
+            return Ok(ritoverzichtRepository.GeefAlleRitoverzichten());
+        }
+    }
+}
---
+++ CovaDISAutoAPI.Domain/Entities/Ritoverzicht.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CovaDISAutoAPI.Domain.Entities
+{
+    internal class Ritoverzicht
+    {
+        public int ID { get; set; }
+        public int Kilometerstand_voor { get; set; }
+        public int Chauffeur_id { get; set; }
+        public int Route_id { get; set; }
+    }
+}
